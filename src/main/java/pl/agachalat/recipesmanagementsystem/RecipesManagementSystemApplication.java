f552c56923b1af5d2b778e3f1665ce405e10a21f
Agnieszka Chalat <kontakt@akramarz.de>
2022-06-08 10:41:57 +0200

    First commit

---
+++ src/main/java/pl/agachalat/recipesmanagementsystem/RecipesManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.agachalat.recipesmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipesManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/agachalat/recipesmanagementsystem/RecipesManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.agachalat.recipesmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
