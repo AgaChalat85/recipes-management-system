d1b85e692cc9c4045b05c998ad92713723442074
Agnieszka Chalat <kontakt@akramarz.de>
2022-06-20 11:05:45 +0200

    Added Tests for TastyController

---
+++ src/test/java/pl/agachalat/recipesmanagementsystem/controller/TastyControllerTest.java
@@ -0,0 +1,14 @@
+package pl.agachalat.recipesmanagementsystem.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+
+@SpringJUnitWebConfig
+@WebMvcTest(TastyController.class)
+public class TastyControllerTest {
+
+
+
+
+
+}
